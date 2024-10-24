1663a3ca3c29fed88c79c4485e3807189bda94ae
saba farzanegan <saba.far.99@gmail.com>
2024-10-24 14:53:20 +0330

    add userrole

--- src/page/Onboarding.tsx
+++ src/page/Onboarding.tsx
@@ -1,5 +1,53 @@
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Onboarding() {
-  return <div>Onboarding</div>;
+  const { user } = useUser();
+  const navigate = useNavigate();
+  const handleRoleuser = async (role: string) => {
+    await user?.update({
+      unsafeMetadata: { role },
+    });
+    navigate(role === "candidate" ? "/jobs" : "/post-jobs");
+  };
+  useEffect(() => {
+    if (user?.unsafeMetadata.role) {
+      navigate(
+        user.unsafeMetadata.role === "candidate" ? "/jobs" : "/post-jobs"
+      );
+    }
+  }, [user]);
+  return (
+    <main
+      className="flex flex-col justify-center
+     items-center gap-y-16">
+      <div className="flex items-center justify-center">
+        <h1 className="gradient-title font-bold text-xl md:text-4xl lg:text-6xl ">
+          ...من هستم یک
+        </h1>
+      </div>
+      <div className="flex items-center justify-center gap-x-6">
+        <Button
+          size="xl"
+          variant="destructive"
+          onClick={() => {
+            handleRoleuser("candidate");
+          }}>
+          کارفرما
+        </Button>
+        <Button
+          size="xl"
+          variant="default"
+          onClick={() => {
+            handleRoleuser("recuiter");
+          }}>
+          کارجو
+        </Button>
+      </div>
+    </main>
+  );
 }
 
 export default Onboarding;
